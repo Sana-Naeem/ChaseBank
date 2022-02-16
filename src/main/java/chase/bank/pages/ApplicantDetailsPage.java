97af47171ae517b761e58a2c77cdeeeebd049506
“Sana-Naeem” <sananaeemqa@gmail.com>
2022-02-16 15:40:15 -0500

    02162022 3:40pm

--- src/main/java/chase/bank/pages/ApplicantDetailsPage.java
+++ src/main/java/chase/bank/pages/ApplicantDetailsPage.java
@@ -43,6 +43,22 @@ public class ApplicantDetailsPage {
 	public WebElement enterPhoneNum;
 	@FindBy(xpath = "//input[@id='input-socialSecurityAdministrationDisclosureAcceptance']")
 	public WebElement authorizationCheckBox;
+	@FindBy(xpath = "//select[@id='select-residenceOwnership-select-validate']")
+	public WebElement selectTypeOfResidence;
+	@FindBy(xpath = "//select[@id='select-primaryIncomeSourceName-select-validate']")
+	public WebElement selectIncome;
+	@FindBy(xpath = "//input[@id='grossAnnualIncome-text-validate-input-field']")
+	public WebElement enterGrossIncome;
+	@FindBy(xpath = "//input[@id='input-paperlessStatements-radio-0']")
+	public WebElement goPaperlessButton;
+	@FindBy(xpath = "//input[@id='input-authorizedUsersHeader-radio-1']")
+	public WebElement addUserButton;
+	@FindBy(xpath = "//input[@id='input-balanceTransfer-radio-1']")
+	public WebElement balanceTranserButton;
+	@FindBy(xpath = "//input[@name='navigationAdvisoryCheckbox']")
+	public WebElement agreementButton;
+	@FindBy(xpath = "//mds-button[@id='SUBMIT-nav-ctr-btn']")
+	public WebElement submitButton;
 	
 	private void enterFirstNameStep(String firstName) {
 		Base_class.common_methods.scrollIntoViewTheElement(enterFirstName);
@@ -105,7 +121,39 @@ public class ApplicantDetailsPage {
 		Base_class.common_methods.selectRadioButton(authorizationCheckBox);
 	}
 	
-	public void applicantInfoSteps(String firstName, String lastName, String suffixValue, String dob, String mothersName, String taxID, String socialSecurityNum, String addressType, String streetAddress, String zipCode, String city, String state, String email, String phoneNum) {
+	private void residenceStep(String residence) {
+		Base_class.common_methods.selectDropDown(selectTypeOfResidence, residence);
+	}
+	
+	private void incomeStep(String income) {
+		Base_class.common_methods.selectDropDown(selectIncome, income);
+	}
+	
+	private void grossIncomeStep(String grossIncome) {
+		Base_class.common_methods.writeText(enterGrossIncome, grossIncome);
+	}
+	
+	private void goPaperlessStep() {
+		Base_class.common_methods.selectRadioButton(goPaperlessButton);
+	}
+	
+	private void addUserStep() {
+		Base_class.common_methods.selectRadioButton(addUserButton);
+	}
+	
+	private void balanceTransferStep() {
+		Base_class.common_methods.selectRadioButton(balanceTranserButton);
+	}
+	
+	private void agreementStep() {
+		Base_class.common_methods.selectRadioButton(agreementButton);
+	}
+	
+	private void clickSumbitButtonStep() {
+		Base_class.common_methods.click(submitButton);
+	}
+	
+	public void applicantInfoSteps(String firstName, String lastName, String suffixValue, String dob, String mothersName, String taxID, String socialSecurityNum, String addressType, String streetAddress, String zipCode, String city, String state, String email, String phoneNum, String residence, String income, String grossIncome) {
 		enterFirstNameStep(firstName);
 		enterLastNameStep(lastName);
 		selectSuffixStep(suffixValue);
@@ -121,6 +169,15 @@ public class ApplicantDetailsPage {
 		enterEmailStep(email);
 		enterPhoneNumStep(phoneNum);
 		authorizationStep();
+		residenceStep(residence);
+		incomeStep(income);
+		grossIncomeStep(grossIncome);
+		goPaperlessStep();
+		addUserStep();
+		balanceTransferStep();
+		agreementStep();
+		clickSumbitButtonStep();
+	
 	}
 	
 }
--- src/test/java/chase/bank/tests/Testing.java
+++ src/test/java/chase/bank/tests/Testing.java
@@ -11,7 +11,7 @@ public class Testing extends Base_class {
 		creditCardsPage.creditCardsPageSteps();
 		applyNowPage.applyNowPageSteps();
 		freedomUnlimitedAppPage.FreedomUnlimitedSteps("Let's get started.");
-		applicantDetailsPage.applicantInfoSteps("Natalie", "Lopez", "JR", "01/02/1994", "Ashley Lopez", "SOCIAL_SECURITY_NUMBER", "000-11-2222", "DOMESTIC", "2301 Westchester Ave", "10462", "Bronx", "NY", "Jasminelopez@hotmail.com", "123-321-1234");
+		applicantDetailsPage.applicantInfoSteps("Natalie", "Lopez", "JR", "01/02/1994", "Ashley Lopez", "SOCIAL_SECURITY_NUMBER", "000-11-2222", "DOMESTIC", "2301 Westchester Ave", "10462", "Bronx", "NY", "Jasminelopez@hotmail.com", "123-321-1234", "RENT", "EMPLOYED","50,000");
 
 	}
 }
